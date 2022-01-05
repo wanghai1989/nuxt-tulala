8c67ee64bcf9fb052287ac76fb501f6e1a398a62
15919483126 <349103751@qq.com>
2022-01-05 18:23:25 +0800

    bug

--- components/mobile/active-invite.vue
+++ components/mobile/active-invite.vue
@@ -37,7 +37,7 @@ export default {
       // this.wxShareCheck()
   },
   mounted(){
-      this.wxShareCheck()
+      // this.wxShareCheck()
     // if(processImg.is_weixn()){
     //   alert(111)
     //   this.wechatShare()
@@ -45,72 +45,72 @@ export default {
     // this.getPromote()
   },
 	methods:{
-    ...mapActions({
-          'shareSdk':'shareSdk'
-      }),
+    // ...mapActions({
+    //       'shareSdk':'shareSdk'
+    //   }),
 
     //检查是否是微信端，是微信端则调用微信分享功能
-    wxShareCheck(){
-      this.getWxShare()
-        let ua = navigator.userAgent.toLowerCase();  
-        if (ua.match(/MicroMessenger/i) == "micromessenger") {
-            this.getWxShare()
-        }        
-    },
+    // wxShareCheck(){
+    //   this.getWxShare()
+    //     let ua = navigator.userAgent.toLowerCase();  
+    //     if (ua.match(/MicroMessenger/i) == "micromessenger") {
+    //         this.getWxShare()
+    //     }        
+    // },
      //微信分享功能调用
-    getWxShare(){
-        // let url =encodeURIComponent(window.location.href.split('#')[0])
-        // console.log(url)
-         let params={
-          'url':'http://www.91tula.com/m/mobile-register'
-        }
+//     getWxShare(){
+//         // let url =encodeURIComponent(window.location.href.split('#')[0])
+//         // console.log(url)
+//          let params={
+//           'url':'http://www.91tula.com/m/mobile-register'
+//         }
 
-       this.shareSdk(params).then(res=>{
-                let wxInfo=res.data
-                wx.config({
-                    debug: false, // 是否开启调试模式
-                    appId: wxInfo.appId, //appid:需与公众号一致
-                    timestamp: wxInfo.timestamp, // 时间戳
-                    nonceStr: wxInfo.nonceStr, // 随机字符串
-                    signature: wxInfo.signature, // 签名
-                    jsApiList: [
-                        'onMenuShareAppMessage',
-                        'onMenuShareTimeline',
-                        'updateAppMessageShareData',
-                        'updateTimelineShareData',
-                    ] // 需要使用的JS接口列表
-                })
-                wx.ready(()=>{  
-                    wx.onMenuShareAppMessage({ 
-                        title: '这是标题',   // 分享时的标题
-                        desc:'小标签',
-                        link: 'http://www.91tula.com/m/mobile-register',     // 分享时的链接
-                        imgUrl: 'http://www.91tula.com/_nuxt/img/logo02.8cf787f.png',    // 分享时的图标
-                        success: function () {
-                            console.log("成功")
-                        },
-                        cancel:function(){
-                            console.log("取消")
-                        }
-                    })
-                    wx.onMenuShareTimeline({ 
-                        title: '这是标题',   // 分享时的标题
-                        link: 'http://www.91tula.com/m/mobile-register',     // 分享时的链接
-                        imgUrl: 'http://www.91tula.com/_nuxt/img/logo02.8cf787f.png',    // 分享时的图标
-                        success: function () {
-                            console.log("成功")
-                        },
-                        cancel:function(){
-                            console.log("取消")
-                        }
-                    })
-                })
-                wx.error(function(res){
-                    console.log(res.errMsg)
-                })
+//        this.shareSdk(params).then(res=>{
+//                 let wxInfo=res.data
+//                 wx.config({
+//                     debug: false, // 是否开启调试模式
+//                     appId: wxInfo.appId, //appid:需与公众号一致
+//                     timestamp: wxInfo.timestamp, // 时间戳
+//                     nonceStr: wxInfo.nonceStr, // 随机字符串
+//                     signature: wxInfo.signature, // 签名
+//                     jsApiList: [
+//                         'onMenuShareAppMessage',
+//                         'onMenuShareTimeline',
+//                         'updateAppMessageShareData',
+//                         'updateTimelineShareData',
+//                     ] // 需要使用的JS接口列表
+//                 })
+//                 wx.ready(()=>{  
+//                     wx.onMenuShareAppMessage({ 
+//                         title: '这是标题',   // 分享时的标题
+//                         desc:'小标签',
+//                         link: 'http://www.91tula.com/m/mobile-register',     // 分享时的链接
+//                         imgUrl: 'http://www.91tula.com/_nuxt/img/logo02.8cf787f.png',    // 分享时的图标
+//                         success: function () {
+//                             console.log("成功")
+//                         },
+//                         cancel:function(){
+//                             console.log("取消")
+//                         }
+//                     })
+//                     wx.onMenuShareTimeline({ 
+//                         title: '这是标题',   // 分享时的标题
+//                         link: 'http://www.91tula.com/m/mobile-register',     // 分享时的链接
+//                         imgUrl: 'http://www.91tula.com/_nuxt/img/logo02.8cf787f.png',    // 分享时的图标
+//                         success: function () {
+//                             console.log("成功")
+//                         },
+//                         cancel:function(){
+//                             console.log("取消")
+//                         }
+//                     })
+//                 })
+//                 wx.error(function(res){
+//                     console.log(res.errMsg)
+//                 })
            
-        }).catch()
-    },
+//         }).catch()
+//     },
 
       getPromote:function(){
         // With async/await
