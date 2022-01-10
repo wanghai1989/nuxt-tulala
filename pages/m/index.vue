8cd7b76ef6610af225652bf4273b2f7c2f8a7ad9
15919483126 <349103751@qq.com>
2022-01-10 09:57:43 +0800

    bug

--- pages/m/index.vue
+++ pages/m/index.vue
@@ -16,10 +16,10 @@ mounted(){
    let shareData={
         title: '这是标题',   // 分享时的标题
         desc:'小标签',
-        link: 'http://www.91tula.com/m/mobile-register',     // 分享时的链接
-        imgUrl: 'http://www.91tula.com/_nuxt/img/logo02.8cf787f.png',    // 分享时的图标
+        link: 'https://www.91tula.com/m/mobile-register',     // 分享时的链接
+        imgUrl: 'https://www.91tula.com/_nuxt/img/logo02.8cf787f.png',    // 分享时的图标
       }
-      let url='http://www.91tula.com'
+      let url='https://www.91tula.com'
       let ua = navigator.userAgent.toLowerCase();  
         if (ua.match(/MicroMessenger/i) == "micromessenger") {
             this.wxShare(shareData,url)
