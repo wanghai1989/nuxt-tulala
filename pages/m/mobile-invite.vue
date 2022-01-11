9ce08681c8fb0d2c31f52a53037e980f97b8f494
15919483126 <349103751@qq.com>
2022-01-11 16:15:57 +0800

    bug

--- pages/m/mobile-invite.vue
+++ pages/m/mobile-invite.vue
@@ -11,8 +11,8 @@ components: {activeInvite},
    let shareData={
         title: '这是标题',   // 分享时的标题
         desc:'小标签',
-        link: 'http://www.91tula.com/m/mobile-register',     // 分享时的链接
-        imgUrl: 'http://www.91tula.com/_nuxt/img/logo02.8cf787f.png',    // 分享时的图标
+        link: 'https://www.91tula.com/m/mobile-register',     // 分享时的链接
+        imgUrl: 'https://www.91tula.com/_nuxt/img/logo02.8cf787f.png',    // 分享时的图标
       }
       let url='https://www.91tula.com/m/mobile-invite'
       let ua = navigator.userAgent.toLowerCase();  
