2ce2423ee7395e6f13a5b1acd619b562b3d559c4
15919483126 <349103751@qq.com>
2022-01-12 16:42:59 +0800

    bug

--- components/task/task-list.vue
+++ components/task/task-list.vue
@@ -5,21 +5,23 @@
 				<div class="center-ctn-box">
 					<div class="table-cell-box">
 						<div class="Bigimg-box">
-							<h3 class="steptheme">工单操作流程</h3>
+							<h2 class="steptheme">工单操作流程</h2>
 							<em class="Bigimg-close-Btn" @click="closeBigImg()">×</em>
-							<p class="step-tit">第一步、任务——>免费发布任务 <span class="cgray f14">（雇主）</span></p>
+							<div class="step-cont">
+							<h3 class="step-tit"><span class="icon_step"></span><em>STEP 1</em> 任务——>免费发布任务 <span class="cgray f14">（雇主）</span></h3>
 							<img src="~/assets/images/work01.jpg" />
-							<p class="step-tit">第二步、任务——>任务名称——>参与竞价<span class="cgray f14">（设计师）</span></p>
+							<h3 class="step-tit"><span class="icon_step"></span><em>STEP 2</em> 任务——>任务名称——>参与竞价<span class="cgray f14">（设计师）</span></h3>
 							<img src="~/assets/images/work02.jpg" />
-							<p class="step-tit">第三步、我的——>我的工单——>我的发布——>安全雇佣<span class="cgray f14">（雇主）</span></p>
+							<h3 class="step-tit"><span class="icon_step"></span><em>STEP 3</em> 我的——>我的工单——>我的发布——>安全雇佣<span class="cgray f14">（雇主）</span></h3>
 							<img src="~/assets/images/work03.jpg" />
-							<p class="step-tit">第四步、我的——>我的工单——>我的发布——>接受雇佣<span class="cgray f14">（设计师）</span></p>
+							<h3 class="step-tit"><span class="icon_step"></span><em>STEP 4</em> 我的——>我的工单——>我的发布——>接受雇佣<span class="cgray f14">（设计师）</span></h3>
 							<img src="~/assets/images/work04.jpg" />
-							<p class="step-tit">第六步、我的——>我的工单——>我的发布——>支付定金<span class="cgray f14">（雇主）</span></p>
+							<h3 class="step-tit"><span class="icon_step"></span><em>STEP 5</em> 我的——>我的工单——>我的发布——>支付定金<span class="cgray f14">（雇主）</span></h3>
 							<img src="~/assets/images/work05.jpg" />
-							<p class="step-tit">第七步、我的——>我的工单——>我的发布——>支付尾款<span class="cgray f14">（雇主）</span></p>
+							<h3 class="step-tit"><span class="icon_step"></span><em>STEP 6</em> 我的——>我的工单——>我的发布——>支付尾款<span class="cgray f14">（雇主）</span></h3>
 							<img src="~/assets/images/work06.jpg" />
-							<p class="step-tit">第八步、后台结算，项目完成</p>
+							<h3 class="step-tit"><em>STEP 7</em> 后台结算，项目完成</h3>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -158,6 +160,15 @@ fetchWorkord:function(){
     vertical-align: middle;
     cursor: pointer;
 }
+.bg-map(@width:25px,@height:25px,@x:0px,@y:0px){
+        display: inline-block;
+        vertical-align: middle;
+        width: @width;
+        height: @height;
+        position: relative;
+        background: url(~/assets/images/bg-ico.png) no-repeat @x @y;
+        cursor: pointer;
+}
 .ul-task{margin-top: 20px;
     li{border-bottom: 1px solid #d6d6d6; padding: 30px 40px; background: #fff;width: 100%; box-sizing: border-box;} 
 li:hover{background: #f9f9f9;}
@@ -187,6 +198,12 @@ li a:hover{text-decoration: underline;}
 .linkrule{display: inline-block; color: var(--color); margin-top: 5px;}
 }
 .nodata{background: #fff; padding: 40px 0;}
-h3.steptheme{font-size: 20px; font-weight: bold; color: var(--backColor); margin: 20px 0px 40px;}
-.step-tit{font-size: 16px; color: var(--backColor); margin: 30px 0px 10px; text-align: left;}
+.step-cont{width: 84%; margin: 0 auto;border-left: 1px dashed #64a0de;}
+.step-cont img{max-width: 90% !important; margin: 0 auto;}
+h2.steptheme{font-size: 20px; font-weight: bold; color: var(--backColor); margin: 20px 0px 40px; }
+.step-tit{font-size: 15px; margin: 30px 0px 10px; color:var(--backColor); text-align: left; padding-left: 37px;position: relative;}
+.icon_step{.bg-map(30px,37px,-116px, -243px);    position: absolute;
+    left: -16px;
+    top: 0px;}
+.step-tit em{font-weight: bold; margin-right: 8px; }
 </style>
