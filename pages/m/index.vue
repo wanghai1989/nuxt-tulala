cf199f2245cbde08ee081e18d86f0d5f58df215f
15919483126 <349103751@qq.com>
2022-01-13 11:48:39 +0800

    bug

--- pages/m/index.vue
+++ pages/m/index.vue
@@ -96,7 +96,7 @@ export default {
 }
 </script>
 <style lang="less" >
-.swiper-container { width: 100%; height: 1.9rem; position: relative;  z-index: 2;  }
+.swiper-container { width: 100%; height: 1.9rem !important; position: relative;  z-index: 2;  }
 .swiper-container img{width: 100%;}
 .swiper-container .swiper-pagination{ bottom: 0px;left: 0; width: 100%;}
 .swiper-pagination-bullet{width: 8px; height: 8px; margin: 0px 4px;}
