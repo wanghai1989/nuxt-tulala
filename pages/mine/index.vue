a42cba0b0e46d56dc8f3e8c99c0e71a99ec47df6
15919483126 <349103751@qq.com>
2022-03-10 17:39:35 +0800

    bug

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -2,6 +2,16 @@
 <div>
     <personBanner></personBanner>
     <personNav></personNav>
+    <div class="laymshade login" v-if="ispublic">
+			<div class="login-box clearfix">
+				<i class="close" @click="close()"></i>
+				<div class="publicNo">
+                    <h3>图啦啦公众号</h3>
+                    <img src="~/assets/images/pic02.jpg"  title="图啦啦公众号"/>
+                    <p>关注公众号及时获取图啦啦最新资讯</p>
+                </div>
+			</div>
+		</div>
     <div class="wrapper">
         <personInfo></personInfo>
     </div>
@@ -20,5 +30,44 @@ components: {personBanner,personNav,personInfo},
 			title: '会员基本信息-图啦啦91tula.com'
 			}
   },
+   data () {
+    return {
+      username: '',
+      password: '',
+      errorMsg: '',
+      ispublic:0  //注册成功，提示关注公众号
+    }
+  },
+ mounted(){
+  if(this.$route.query.publicno){
+     this.ispublic=1
+   }
+  },
+  methods: {
+    close(){
+    this.ispublic=0
+    },
+
+}
 }
 </script>
+<style lang="less" scoped>
+.bg-map(@width:25px,@height:25px,@x:0px,@y:0px){
+        display: inline-block;
+        vertical-align: middle;
+        width: @width;
+        height: @height;
+        position: relative;
+        background: url(../../assets/images/bg-ico.png) no-repeat @x @y;
+        cursor: pointer;
+}
+.login-box{width: 340px; position: fixed; height: 360px; top: 50%; left: 50%; margin-top: -250px; margin-left: -170px; z-index: 5; 
+background: #fff; border-radius: 5px; accelerator: false; position: relative;
+.login{text-align: center; }
+.publicNo{padding: 40px 30px 20px; text-align: center;}
+.publicNo h3{font-size: 20px; text-align: center; font-weight: bold; color: var(--backColor);}
+.publicNo img{width: 220px;}
+.close{.bg-map(24px,24px,-583px, -44px); position: absolute; top: 10px; right: 10px;}
+
+}
+</style>
